ac5a20bd9d1992a8227916d85ae487368f81be1f
hazlomcodes <harry.lomas1@outlook.com>
2023-04-22 13:00:23 +0100

    DogYears added

---
+++ DogYears.js
@@ -0,0 +1,16 @@
+//sets variable myAge to 25
+const myAge = 25;
+//sets mutable varibale earlyYears to value of 2
+let earlyYears = 2;
+// multiplys early years varibale by 10.5 and reassigns value
+earlyYears *= 10.5;
+//takes myAge variable subtracts 2 and stores the value in laterYears variable
+let laterYears = myAge - 2;
+//calculates number of dog years by * later years by 4
+laterYears *= 4;
+// adds earlyYears and laterYears varibale together and assigns total to new varibale myAgeInDogYeats
+myAgeInDogYears = earlyYears + laterYears
+//Returns Harry as lowercase
+myName = 'Harry Lomas'.toLowerCase();
+//prints and uses template literals to display ages dependent on varibale arithmetic
+console.log(`My name is ${myName}. I am ${myAge} in human years which is ${myAgeInDogYears} old in dog years`)
